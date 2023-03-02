c513f619a27080d53ac446ab7f27805f7f40ebca
Felipe Toro Castilla <ftorocastilla@gmail.com>
2023-03-02 18:54:41 -0300

    feat: initialize the project, starting create layouts

---
+++ src/components/Vitrine.jsx
@@ -0,0 +1,5 @@
+export function Vitrine () {
+  return (
+    <h1> Esta es la Vitrina en React</h1>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import { createRoot } from 'react-dom/client'
+import { Vitrine } from './components/Vitrine'
+
+const root = createRoot(document.getElementById('vitrine'))
+root.render(<Vitrine />)
