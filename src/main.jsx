277977a599b6f983ba6af862696fada8242a5032
Felipe Toro Castilla <ftorocastilla@gmail.com>
2023-03-05 13:41:24 -0300

    feat: add section produtos

--- src/main.jsx
+++ src/main.jsx
@@ -3,3 +3,6 @@ import { Vitrine } from './Vitrine'
 
 const root = createRoot(document.getElementById('vitrine'))
 root.render(<Vitrine />)
+
+const root2 = createRoot(document.getElementById('vitrine2'))
+root2.render(<Vitrine />)
