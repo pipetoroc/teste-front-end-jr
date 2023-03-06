2932960f8e667d9e00dd4cd8bc0190971e426674
Felipe Toro Castilla <94933663+pipetoroc@users.noreply.github.com>
2023-03-06 20:22:00 -0300

    Delete src directory

--- src/services/getProducts.js
+++
@@ -1,8 +0,0 @@
-export default function getProducts () {
-  return fetch('https://app.econverse.com.br/teste-front-end/junior/tecnologia/lista-produtos/produtos.json')
-    .then(res => res.json())
-    .then(response => {
-      const { products } = response
-      return products
-    })
-}
