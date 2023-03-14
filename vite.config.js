ef7878003bd87ba78b53e45fb159af1c3e9a44bf
Felipe Toro Castilla <ftorocastilla@gmail.com>
2023-03-14 18:27:07 -0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    // sassPlugin(),
-    react()
-  ]
+    react()],
+  base: '/teste-front-end-jr/'
 })
